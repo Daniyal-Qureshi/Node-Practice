9563f930a4bcd4239508fbec9f2dd491a3118163
Daniyal Qureshi <daniyalqureshi268@gmail.com>
2022-02-01 13:20:58 -0800

    Sds

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const app = express();
 const body_parser = require("body-parser");
 const mongoose = require("mongoose");
-const path = require('path')
+const path = require("path");
 const Product = require("./model/Product");
-// const { redirect } = require("express/lib/response");
-const fileUpload = require('express-fileupload')
+const session = require("express-session");
 
+// const { redirect } = require("express/lib/response");
+const fileUpload = require("express-fileupload");
+const bcrypt = require("bcrypt");
 mongoose.connect(
-    "mongodb://127.0.0.1:27017/store", { useNewUrlParser: true },
-    () => console.log("connected")
+  "mongodb://127.0.0.1:27017/store",
+  { useNewUrlParser: true },
+  () => console.log("connected")
 );
 
 require("ejs");
@@ -17,54 +20,53 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true }));
-app.use(fileUpload())
+app.use(fileUpload());
+app.use(session({ secret: "2323", saveUninitialized: true, resave: true }));
 const User = require("./model/User");
-const { runMain } = require("module");
-
+const authentication = require("./middleware/authentication");
 
 app.get("/signin", (req, res) => res.render("signin"));
 
 app.get("/signup", (req, res) => res.render("signup"));
 
-app.post("/save", async(req, res) => {
-    const user = await User.create(req.body);
+app.post("/save", async (req, res) => {
+  const user = await User.create(req.body);
 
-
-
-
-    console.log(user);
-    res.redirect("/signin");
+  console.log(user);
+  res.redirect("/signin");
 });
 
+app.get("/product", (req, res) => res.render("product"));
 
+app.get("/", authentication, async (req, res) => {
+  const products = await Product.find();
+  console.log("All ", products);
 
-app.get("/product", (req, res) => res.render("product"))
+  res.render("home", { products });
+});
 
-app.get("/", async(req, res) => {
-    const products = await Product.find()
-    console.log("All ", products);
+app.post("/add", async (req, res) => {
+  console.log(req.files);
+  const image = req.files.image;
+  image.mv(path.resolve(__dirname, "./public/img", image.name));
+  await Product.create({ ...req.body, image: image.name });
+  res.redirect("/");
+});
 
+app.post("/auth", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    bcrypt.compare(req.body.password, user.password, (err, same) => {
+      if (same) {
+        req.session.userid = user._id;
 
-    res.render("home", { products })
-
-
-})
-
-app.post("/add", async(req, res) => {
-    console.log(req.files);
-    const image = req.files.image
-    image.mv(path.resolve(__dirname, './public/img', image.name))
-    await Product.create({...req.body, image: image.name })
-    res.redirect("/")
-
-})
-
-app.post("/auth", async(req, res) => {
-    const user = await User.findOne(req.body);
-    if (user) res.render("home");
-    else res.redirect("/signin");
+        console.log(req.session);
+        res.redirect("/");
+      } else res.redirect("/signin");
+    });
+  } else res.redirect("/signin");
 });
 
 app.listen(3000, () => {
-    console.log("listing");
+  console.log("listing");
 });
--- model/User.js
+++ model/User.js
@@ -1,1 +1,11 @@
-const mongoose = require("mongoose"); const userSchema = mongoose.Schema({ email: String, password: String, }); module.exports = mongoose.model("user", userSchema);
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = mongoose.Schema({ email: String, password: String });
+userSchema.pre("save", function (next) {
+  bcrypt.hash(this.password, 5, (err, hash) => {
+    this.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model("user", userSchema);
